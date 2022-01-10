14c5ef5002a29a6ceababfec0d9aaf31097e6990
Fonkarts <82459566+Fonkarts@users.noreply.github.com>
2022-01-10 09:28:19 +0100

    v 0.4 Fonction like/dislike et gestion de la BDD
    - Ajouté la route /:id/like
    - Modifié le modèle de sauce, définition d'une valeur par défaut de "0" pour les likes/dislikes.
    - Création du contrôleur "likeSauce", permettant de:
    -- liker une sauce
    -- disliker
    -- retirer son vote
    -- tout en modifiant les données de la BDD.
    
    - Ajouté les fichiers images au .gitignore
    - Préservé le dossier "images" avec une exception pour le fichier ".gitkeep".

--- back/controllers/sauce.js
+++ back/controllers/sauce.js
@@ -1,5 +1,6 @@
 const Sauce = require("../models/sauces");
 const fs = require("fs");
+// const { parse } = require("path/posix");
 
 exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);
@@ -13,10 +14,42 @@ exports.createSauce = (req, res, next) => {
         .catch(error => res.status(400).json({error}));
 };
 
+exports.likeSauce = (req, res, next) => {
+    const userId = req.body.userId;
+    const like = req.body.like;
+    Sauce.findOne({_id: req.params.id})
+        .then(sauce => {
+            const likedUserIndex = sauce.usersLiked.indexOf(req.body.userId);
+            const dislikedUserIndex = sauce.usersDisliked.indexOf(req.body.userId);
+            if(like==1) {
+                sauce.usersLiked.push(userId);
+                sauce.likes+= 1;
+            } 
+            else if(like==-1) {
+                sauce.usersDisliked.push(userId);
+                sauce.dislikes+= 1;
+            } 
+            else if(like==0 && likedUserIndex !== -1) {
+                sauce.usersLiked.splice(likedUserIndex, 1);
+                sauce.likes-=1; 
+            } 
+            else if(like==0 && dislikedUserIndex !== -1) {
+                sauce.usersDisliked.splice(dislikedUserIndex, 1);
+                sauce.dislikes-=1;
+            } 
+            else {
+            };
+            sauce.save()
+            .then(() => res.status(200).json({message: "ok !"}))
+            .catch(error => res.status(500).json({error}));
+        })
+        .catch(error => res.status(500).json({error}));
+};
+
 exports.modifySauce = (req, res, next) => {
     const sauceObject = req.file ?
     {
-        ...JSON.parse(req.body.Sauce),
+        ...JSON.parse(req.body.sauce),
         imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`
     } : {...req.body};
     Sauce.updateOne({_id: req.params.id}, {...sauceObject, _id: req.params.id})
